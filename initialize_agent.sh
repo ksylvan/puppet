5d453b61329d97a3ab194f95ea887a80e3c98ed8
Kayvan Sylvan <ksylvan@playdom.com>
2013-07-11 10:30:55 -0700

    Improve initialization scripts to guard against running multiple times

--- initialize_agent.sh
+++ initialize_agent.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 #
 echo "=== Puppet Agent initialization..."
-echo sudo puppet agent --verbose --no-daemonize --verbose --onetime --server=master
-sudo puppet agent --verbose --no-daemonize --verbose --onetime --server=master
-echo "=== Please run initialize_master.sh on the master to continue..."
-echo "=== Once you have done that, hit RETURN"
-read x
-echo sudo puppet agent --verbose --no-daemonize --verbose --onetime --server=master
-sudo puppet agent --verbose --no-daemonize --verbose --onetime --server=master
+if [ ! -r /vagrant/.agent ]
+then
+  echo sudo puppet agent --verbose --no-daemonize --verbose --onetime \
+    --server=master
+  sudo puppet agent --verbose --no-daemonize --verbose --onetime --server=master
+
+  echo "=== Please run initialize_master.sh on the master to continue..."
+  echo "=== Once you have done that, hit RETURN"
+  read x
+
+  echo sudo puppet agent --verbose --no-daemonize --verbose --onetime \
+    --server=master
+
+  sudo puppet agent --verbose --no-daemonize --verbose --onetime --server=master
+  touch /vagrant/.agent
+else
+  echo "Looks like agent has already been initialized."
+  echo "Use the following command to do a one-time catalog run:"
+  echo sudo puppet agent --verbose --no-daemonize --verbose --onetime \
+    --server=master
+fi
--- initialize_master.sh
+++ initialize_master.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 #
 echo "=== Pupper Master initialization..."
-echo "=== setting certname to master..."
-sudo su -c 'echo "certname=master" >> /etc/puppet/puppet.conf'
-sudo service puppetmaster restart
-sudo puppetca sign --all
+if [ ! -r /vagrant/.master ]
+then
+  echo "=== setting certname to master..."
+  sudo su -c 'echo "certname=master" >> /etc/puppet/puppet.conf'
+  sudo service puppetmaster restart
+  sudo puppetca sign --all
 
-echo "=== copying the sudo module and manifests/ files..."
-sudo cp /vagrant/puppet/manifests/* /etc/puppet/manifests/
-sudo cp -r /vagrant/puppet/modules/sudo /etc/puppet/modules/
-sudo service puppetmaster restart
+  echo "=== copying the sudo module and manifests/ files..."
+  sudo cp /vagrant/puppet/manifests/* /etc/puppet/manifests/
+  sudo cp -r /vagrant/puppet/modules/sudo /etc/puppet/modules/
+  sudo service puppetmaster restart
+  touch /vagrant/.master
+else
+  echo "Looks like the initialization of master has been run already."
+  echo "Nothing to do."
+fi
